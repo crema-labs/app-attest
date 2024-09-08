b8a0497281c1ebcbaef1b250091614cbc310d48e
0xvikasrushi <0xvikas@gmail.com>
2024-09-08 14:09:23 +0530

    feat: test for utils.circom

---
+++ circuits/utils.circom
@@ -0,0 +1,47 @@
+pragma circom 2.1.9;
+
+include "circomlib/circuits/bitify.circom";
+
+template SplitToWords(nBits, wordsize, numberElement) {
+    signal input in[nBits];
+    signal output out[numberElement];
+
+    assert(numberElement * wordsize == nBits);
+
+    component bitsToNum[numberElement];  
+
+    for (var i = 0; i < numberElement; i++) {
+        bitsToNum[i] = Bits2Num(wordsize);
+        for (var j = 0; j < wordsize; j++) {
+            bitsToNum[i].in[wordsize-1-j] <== in[i*wordsize + j];
+        }
+        bitsToNum[i].out ==> out[numberElement-1-i];
+    }
+}
+
+template BytesToBits(nBytes) {
+    signal input in[nBytes];
+    signal output out[nBytes*8];
+
+    component NumToBits[nBytes];
+
+    for (var i=0; i < nBytes; i++) {
+        NumToBits[i] = Num2Bits(8);
+        NumToBits[i].in <== in[i];
+       for (var j=0; j < 8; j++) {
+            out[i*8 + j] <== NumToBits[i].out[j];
+        }
+    }
+}
+
+template PadBits(nBits,target){
+    signal input in[nBits];
+    signal output out[target];
+
+    for (var i=0; i < target-nBits; i++) {
+        out[i] <== in[i];
+    }
+    for (var i= target-nBits; i < target; i++) {
+        out[i] <== 0;
+    }
+}
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -2,13 +2,13 @@ import { WitnessTester } from "circomkit";
 import crypto from "crypto";
 import { circomkit } from "./common";
 import { hexToBigInt, splitToWords, bufferToBigIntBitArray } from "../src";
-import { log } from "console";
+
 describe("SplitToWords", () => {
   let circuit: WitnessTester<["in"], ["out"]>;
 
   before(async () => {
     circuit = await circomkit.WitnessTester(`SplitToWords`, {
-      file: "attestation",
+      file: "utils",
       template: "SplitToWords",
       params: [384, 48, 8],
     });
@@ -26,5 +26,51 @@ describe("SplitToWords", () => {
         out: words,
       }
     );
+  });
+});
+
+describe("PadBits", () => {
+  let circuit: WitnessTester<["in"], ["out"]>;
+
+  before(async () => {
+    circuit = await circomkit.WitnessTester(`PadBits`, {
+      file: "utils",
+      template: "PadBits",
+      params: [4, 8],
+    });
+    console.log("#constraints:", await circuit.getConstraintCount());
+  });
+
+  it("should pad bits correctly", async () => {
+    const inputBits = [1, 0, 1, 1];
+    const expectedOutput = [1, 0, 1, 1, 0, 0, 0, 0];
+    await circuit.expectPass({ in: inputBits }, { out: expectedOutput });
+  });
+});
+
+function byteToBits(byte: number): number[] {
+  let bits = [];
+  for (let i = 7; i >= 0; i--) {
+    bits.push((byte >> i) & 1);
+  }
+  return bits.reverse();
+}
+
+describe("BytesToBits", () => {
+  let circuit: WitnessTester<["in"], ["out"]>;
+  const testBytes = [0xa5, 0x53, 0x21, 0x12];
+
+  before(async () => {
+    circuit = await circomkit.WitnessTester(`BytesToBits`, {
+      file: "utils",
+      template: "BytesToBits",
+      params: [testBytes.length],
+    });
+    console.log("#constraints:", await circuit.getConstraintCount());
+  });
+
+  it("should convert bytes to bits", async () => {
+    const expectedBits = testBytes.map((byte) => byteToBits(byte)).flat();
+    await circuit.expectPass({ in: testBytes }, { out: expectedBits });
   });
 });
